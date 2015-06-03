5d8daf9fd56229a4b562b539fbc2b18f2210694b
uobkan <nak.bou04@gmail.com>
2015-06-02 22:47:56 -0700

    Move code into functions and define display functions inside JSON objects.

--- js/helper.js
+++ js/helper.js
@@ -50,8 +50,8 @@ var HTMLschoolLocation = '<div class="location-text">%data%</div>';
 var HTMLschoolMajor = '<em><br>Major: %data%</em>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineTitle = '<a href="#">%data%</a>';
+var HTMLonlineSchool = '<a href="#">%data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
